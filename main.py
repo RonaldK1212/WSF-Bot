6792316d715c9031cd5412d99dc84b698da6e9b9
Georges Kfoury <georgeskfoury12x@icloud.com>
2023-06-16 05:03:59 +0300

    changed the random reply behavior

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ import random
 import gpt
 
 # Get the guild ID from the environment variables
-guild = discord.Object(id=os.getenv('GUILD_ID'))
+guild = discord.Object(id=os.getenv("GUILD_ID"))
 
 # Open and load the slurs.json file
-with open('slurs.json') as f:
+with open("slurs.json") as f:
     slurs_file = json.load(f)
 slurs = slurs_file["slurs"]
 
+
 # Define a custom client class
-
-
 class MyClient(discord.Client):
     def __init__(self):
         # Initialize the client with the default intents
         super().__init__(intents=discord.Intents.default())
         self.synced = False
+        self.consecutive_messages = 0
 
     async def startup(self):
         # Wait until the client is ready
@@ -35,8 +35,8 @@ class MyClient(discord.Client):
             self.synced = True
             print("Synced.")
 
-        print(f'Logged on as {self.user}!')
-
+        print(f"Logged on as {self.user}!")
+    
     async def on_message(self, message):
         # Ignore messages sent by the bot itself
         if message.author == self.user:
@@ -47,21 +47,29 @@ class MyClient(discord.Client):
 
         # If the bot is mentioned and GPT is enabled
         if client.user in message.mentions and config.gpt_enabled:
+            # Reset the consecutive message count if the bot replies
+            self.consecutive_messages = 0
+            
             # If the message is a reply to the bot's message
             if message.reference and message.reference.resolved.author == self.user:
-                assistant_message = {"role": "assistant",
-                                     "content": message.reference.resolved.content}
+                assistant_message = {"role": "assistant", "content": message.reference.resolved.content}
                 user_message = {"role": "user", "content": message.content}
-                response, _, _ = gpt.send_message(
-                    [assistant_message, user_message], model="gpt-4")
+                response, _, _ = gpt.send_message([assistant_message, user_message], model="gpt-4")
             else:
-                user_message = {"role": "user", "content": message.content}
-                response, _, _ = gpt.send_message(
-                    [user_message], model="gpt-4")
+                user_message = {"role": "user","content": message.content}
+                response, _, _ = gpt.send_message([user_message], model="gpt-4")
             await message.channel.send(response)
-        # If the random number is less than or equal to 3, reply with a random slur
-        elif x <= 3:
+
+        # If the random number is less than or equal to the current chance, reply with a random slur
+        elif x <= (3 + 5 * self.consecutive_messages): # 3% base + 5% per message
             await message.reply(random.choice(slurs))
+            
+            # Increase the consecutive message count
+            self.consecutive_messages += 1
+
+        else:
+            # Reset the consecutive message count
+            self.consecutive_messages = 0
 
 
 # Create an instance of the custom client
@@ -70,16 +78,19 @@ client = MyClient()
 # Create a command tree for the client
 tree = app_commands.CommandTree(client)
 
+
 # Define a command for the command tree
 @tree.command(name="gay", description="Command description.", guild=guild)
 async def commandName(interaction: discord.Interaction, member: discord.Member = None):
     # Send a message when the command is used
     await interaction.response.send_message("Matthew")
 
+
 @tree.command(name="Translator", description="Command description.", guild=guild)
 async def commandName(interaction: discord.Interaction, member: discord.Member = None):
     # Send a message when the command is used
     await interaction.response.send_message("Work in progress!!!")
 
+
 # Run the client with the bot token from the environment variables
-client.run(os.getenv('BOT_TOKEN'))
+client.run(os.getenv("BOT_TOKEN"))
